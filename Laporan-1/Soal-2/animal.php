13e027f7f3c7f29879e90ff162ae480e03773a44
Rizkyputripratiwi <99009780+Rizkyputripratiwi@users.noreply.github.com>
2023-10-17 05:35:18 +0700

    Revisi Laporan 1 Belum Selesai

--- Laporan-1/Soal-2/animal.php
+++ Laporan-1/Soal-2/animal.php
@@ -1,11 +1,11 @@
 <?php
 
 // Mendefinisikan kelas Animal
-class Animal {
+abstract class Animal {
     // Properti publik untuk kelas Animal
     public $legs = 4; // Jumlah kaki (nilai default)
     public $name; // Nama hewan (akan diatur saat objek dibuat)
-    public $cold_blooded = "no"; // Apakah berdarah dingin (nilai default)
+    public $is_cold_blooded = false; // Apakah berdarah dingin (nilai default)
 
     // Konstruktor kelas Animal, menerima parameter $nama untuk mengatur nama hewan
     public function __construct($nama) {
@@ -13,10 +13,7 @@ class Animal {
     }
 
     // Metode yell() mengembalikan suara yang spesifik untuk hewan ini
-    public function yell() {
-        return "Mbee Mbee";
-    }
+    abstract public function yell();
 }
-
 
 ?>
